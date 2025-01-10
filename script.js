e7e32a0c12b456f95d987d4ad01865e15d535560
Ben LeQuire <benlequire@gmail.com>
2025-01-10 13:45:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function send_to_server(){
     //console.log("this worked!!!")
     var name = document.getElementById("name").value;
-    var message = document.getElementById("messages").value;
+    var message = document.getElementById("send").value;
     console.log(name + " ")
-    console.log(massage+ " ")
+    console.log("\n" + message+ " ")
 }
