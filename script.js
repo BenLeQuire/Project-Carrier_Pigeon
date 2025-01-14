6fd57c4220281cd1a11579c3577a5153edc2a1a6
Ben LeQuire <benlequire@gmail.com>
2025-01-14 09:24:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,8 @@ function send_to_server(){
     //console.log("this worked!!!")
     var name = document.getElementById("name").value;
     var message = document.getElementById("send").value;
-    const texarea = document.getElementById("messages").value;
-    console.log(texarea)
-    //texarea.value += (name+" "+"--->"+" "+message);
-    //console.log("\n"+ name);
-    //console.log("\n" + message);
+    var chat = document.getElementById("messages");
+    console.log(chat)
+    chat.value += name +" "+"--->"+" "+ message + "\n";
+
 }
